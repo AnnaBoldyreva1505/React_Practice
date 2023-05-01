57d6cf1da808437a232b79c1de3c3d971c5c92bf
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-02 00:37:27 +0600

    bad

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import 'modern-normalize/modern-normalize.css';
+// import './styles.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import {ThemeProvider} from 'styled-components'
-//если используется стайл компонентс нужно использовать тимизацию (Theming) https://styled-components.com/docs/advanced#theming
-
-const theme = {
-colors: {
-  black: '#252525',
-  white: '#ffffff',
-  accent: 'tomato',
-},
-radii: {
-  sm: '2px',
-  md: '4px',
-  lg: '8px',
-},
-}
+import { App } from './App';
+import 'modern-normalize/modern-normalize.css';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <ThemeProvider theme={theme}>
     <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
