aa93eb4949c9be89d35124f8411b86c7f9292d48
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-05 03:09:25 +0600

    qwerg

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -2,12 +2,13 @@ import PropTypes from 'prop-types';
 import { Recipe } from '../Recipe/Recipe';
 import { List, ListItem } from './RecipeList.styled';
 
-export const RecipeList = ({ items }) => {
+export const RecipeList = ({ items, onDelete }) => {
   return (
     <List>
+      {/* мы передали onDelete пропсом сюда, но этот пропс не нужен этому фйлу, его нужно прокинуть дальше, в Recepe. Для этого можно передать его просто с темже названием. */}
       {items.map(item => (
         <ListItem key={item.id}>
-          <Recipe item={item} />
+          <Recipe item={item} onDelete={onDelete}/>
         </ListItem>
       ))}
     </List>
