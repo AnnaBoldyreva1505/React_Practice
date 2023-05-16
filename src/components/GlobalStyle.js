6edb12a43f5128343510afb89b49f5ad102cbdab
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-17 01:32:06 +0600

    add practice

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,8 +1,7 @@
-import { createGlobalStyle } from "styled-components"
-import 'modern-normalize'
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
-
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -11,22 +10,25 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
 h1,h2,h3,h4,h5,p {
   margin: 0;
 }
+
 ul, ol {
   margin: 0;
   padding: 0;
   list-style: none;
 }
+
 img {
   display: block;
   max-width: 100%;
   object-fit: cover;
 }
-
-`
+`;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-max-width: 960px;
-padding: 0 16px;
-display: flex;
-flex-direction: column;
-gap: 16px;
-`
-
-export const Layout = ({children}) => {
-    return <Wrapper>
-    <header>PAGE HEADER</header>
-    <main>{children}</main>
-    <footer>PAGE FOOTER</footer>
-    </Wrapper>
-}
+export const Layout = styled.div`
+  display: grid;
+  gap: 16px;
+  padding: 16px;
+`;
